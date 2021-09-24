dcf3e8997767ab159c6e605229c1cfc067c15f4c
Iskandar <iskandar200213@gmail.com>
2021-09-24 15:38:57 +0300

    added two-fer & test fix

---
+++ bin/two_fer/two_fer.dart
@@ -0,0 +1,9 @@
+import 'package:dart_on_exercism/two_fer/two_fer.dart';
+
+void main(){
+
+  String res = twoFer();
+  print(res);
+
+
+}
---
+++ lib/two_fer/two_fer.dart
@@ -0,0 +1,4 @@
+String twoFer([String str = "you"]) {
+
+  return 'One for $str, one for me.';
+}
